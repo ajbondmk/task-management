54ba821d2a10dbb2f803e40040d760ec48d4c98d
Aliyah Bond <ajbondmk@hotmail.co.uk>
2025-06-27 05:07:08 +0100

    Add comments

--- task_runner/task_runner.sh
+++ task_runner/task_runner.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# If the database table exists, then:
+# - Increase all running tasks' progress by 1 to 5 percent (randomly), capped at 100%.
+# - Set all tasks that have just reached 100% to have 'completed' status and some fake results.
 sleep 10
 while true; do
     if [ $(PGPASSWORD="$DATABASE_PASSWORD" psql -tA -h "$DATABASE_HOST" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -c "SELECT to_regclass('public.app_task');") = "app_task" ]; then
