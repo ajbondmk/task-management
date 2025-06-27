1491610a531d853513b4f018ff9bbc484ad54a13
Aliyah Bond <ajbondmk@hotmail.co.uk>
2025-06-27 04:43:50 +0100

    Make sure DB initialises correctly in a fresh container

--- task_runner/task_runner.sh
+++ task_runner/task_runner.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sleep 10
 while true; do
-    PGPASSWORD="$DATABASE_PASSWORD" psql -q -h "$DATABASE_HOST" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -c "
-        UPDATE app_task SET \"progressPercentage\"=LEAST(\"progressPercentage\" + floor(random() * 5 + 1), 100) WHERE status=2;
-        UPDATE app_task SET status=3, results='These are your results!' WHERE \"progressPercentage\"=100 AND status=2;"
+    if [ $(PGPASSWORD="$DATABASE_PASSWORD" psql -tA -h "$DATABASE_HOST" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -c "SELECT to_regclass('public.app_task');") = "app_task" ]; then
+        PGPASSWORD="$DATABASE_PASSWORD" psql -h "$DATABASE_HOST" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -c "
+            UPDATE app_task SET \"progressPercentage\"=LEAST(\"progressPercentage\" + floor(random() * 5 + 1), 100) WHERE status=2;
+            UPDATE app_task SET status=3, results='These are your results!' WHERE \"progressPercentage\"=100 AND status=2;"
+    fi
     sleep 1
 done
