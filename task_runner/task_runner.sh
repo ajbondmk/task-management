c8895b82a7b9a2a64a48cbcc59c5b8bd06ab9f28
Aliyah Bond <ajbondmk@hotmail.co.uk>
2025-06-27 01:07:39 +0100

    Add randomness to task progress :)

--- task_runner/task_runner.sh
+++ task_runner/task_runner.sh
@@ -2,7 +2,7 @@
 
 while true; do
     PGPASSWORD="$DATABASE_PASSWORD" psql -h "$DATABASE_HOST" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -c "
-        UPDATE app_task SET \"progressPercentage\"=LEAST(\"progressPercentage\"+3, 100) WHERE status=2;
+        UPDATE app_task SET \"progressPercentage\"=LEAST(\"progressPercentage\" + floor(random() * 5 + 1), 100) WHERE status=2;
         UPDATE app_task SET status=3, results='These are your results!' WHERE \"progressPercentage\"=100 AND status=2;"
     sleep 1
 done
